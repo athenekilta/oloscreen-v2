a5a4d2cad7b894d0461dcc4b7e57c22314b50702
Kasper Nurminen <kasper@taiste.fi>
2019-02-24 11:38:14 +0200

    Changed title to oloscreen v2

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <h1>
+            Oloscreen v2
+          </h1>
           <a
             className="App-link"
             href="https://reactjs.org"
