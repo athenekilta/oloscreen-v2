2882f2ebd7f49696b28465314a621b3ed6d2c568
Ilari Kuoppala <ilari@kuoppa.la>
2019-02-24 13:46:43 +0200

    Temporarily use 25.2. sodexo menus

--- menuproxy/menuproxy.py
+++ menuproxy/menuproxy.py
@@ -8,6 +8,9 @@ app = Flask(__name__)
 @app.route('/restaurants/')
 def restaurants():
     timestamp = datetime.now().strftime("%Y/%m/%d")
+    # Use monday 25.2.19 for testing purposes
+    if timestamp == '2019/02/24':
+        timestamp = "2019/02/25"
     sodexo_url = f'https://kitchen.kanttiinit.fi/restaurants/2/menu?day={timestamp}/'
     resp = make_response(jsonify({'sodexo': requests.get(sodexo_url).json()}))
     resp.headers['Access-Control-Allow-Origin'] = '*'
