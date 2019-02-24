bb40cc0556431300609bcd9930d365463cef4fee
Kasper Nurminen <kasper@taiste.fi>
2019-02-24 14:32:57 +0200

    Added amica and subway data

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import '../App.css';
 import moment from 'moment';
+import 'moment/locale/fi'
 import axios from 'axios'
 
 
 
-
+moment.locale("fi")
 class MenuComponent extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            sodexoData: null
+            restaurantData: null
         }
 
     }
@@ -20,18 +21,76 @@ class MenuComponent extends Component {
         let self = this
         axios.get(apiURL)
             .then(function (response) {
-                self.setState({ sodexoData: response.data })
+                self.setState({ restaurantData: response.data })
             })
     }
-    render() {
-        const { sodexoData } = this.state
-        if (!sodexoData) { return null }
-        console.log(sodexoData)
+    renderSodexoMenu(restaurantData) {
+        const openingHours = restaurantData.openingHours[moment().weekday() - 2] || "Suljettu tänään"
+        const courses = restaurantData.menus[0].courses.map(course => {
+            return <p>{course.title}</p>
+        })
+
         return (
-            <div className="food-menu">
-
-
+            <div>
+                <h2> T-Talo ({openingHours})</h2>
+                {courses}
             </div>
+
+
+        )
+    }
+    renderSubwayMenu(restaurantData) {
+        const openingHours = "EMT"
+
+
+        return (
+            <div>
+                <h2> Subway ({openingHours})</h2>
+                {restaurantData.title}
+            </div>
+
+
+        )
+    }
+    renderAmicaMenu(restaurantData) {
+        console.log(restaurantData)
+        const openingHours = restaurantData.MenusForDays[0].LunchTime
+        const courses = restaurantData.MenusForDays[0].SetMenus.map(course => {
+            const courseName = course.Name.toLowerCase()
+            if (courseName.includes("lounas")) {
+                return (
+                    <div>
+                        <p>{course.Components.join(", ")}</p>
+                    </div>
+                )
+            }
+            else if (courseName.includes("bufee")) {
+                return (
+                    <div>
+                        <h3>Salaattibuffa</h3>
+                        <p>{course.Components.join(", ")}</p>
+                    </div >
+                )
+            }
+
+        })
+        return (
+            <div>
+                <h2> TUAS ({openingHours})</h2>
+                {courses}
+            </div>
+        )
+    }
+    render() {
+        const { restaurantData } = this.state
+        if (!restaurantData) { return null }
+        console.log(restaurantData)
+        return (
+            <div className="food-menu" >
+                {this.renderSodexoMenu(restaurantData.sodexo)}
+                {this.renderAmicaMenu(restaurantData.amica)}
+                {this.renderSubwayMenu(restaurantData.subway)}
+            </div >
         );
     }
 }
