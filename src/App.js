68bccaf3fb99ba7e410a6c431b1cc92661c88cab
Katri Saarinen <katri@Katri-Saarinen-MacBook.local>
2019-02-24 19:17:56 +0200

    Working countdown without styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import HeaderComponent from './components/Header.js'
+import HeaderComponent from './components/Header.js';
+import Countdown from './components/Countdown.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <HeaderComponent />
+        <Countdown />
       </div>
     );
   }
---
+++ src/components/Countdown.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import '../App.css';
+import moment from 'moment';
+
+class Countdown extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: []
+    };
+    this.counter = this.counter.bind(this);
+  }
+
+  counter(eventDate) {
+    const countDownDate = moment(eventDate).valueOf();
+    const now = moment();
+    const distance = countDownDate - now;
+
+    const pad = num => {
+      let s = `${num}`;
+      while (s.length < 2) {
+        s = `0${s}`;
+      }
+      return s;
+    };
+
+    const days = pad(Math.floor(distance / (1000 * 60 * 60 * 24)));
+    const hours = pad(
+      Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+    const minutes = pad(
+      Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+    );
+    const seconds = pad(Math.floor((distance % (1000 * 60)) / 1000));
+
+    return <p>{days + ' ' + hours + ' ' + minutes + ' ' + seconds}</p>;
+  }
+
+  componentDidMount = () => {
+    this.getEvents();
+
+    this.interval = setInterval(() => {
+      this.counter();
+      // setting the state re-renders the page
+      this.setState({ done: true });
+    }, 1000);
+  };
+
+  getEvents() {
+    let that = this;
+    function start() {
+      window.gapi.client
+        .init({
+          apiKey: process.env.REACT_APP_GOOGLE_API_KEY
+        })
+        .then(function() {
+          return window.gapi.client.request({
+            path: `https://www.googleapis.com/calendar/v3/calendars/${
+              process.env.REACT_APP_GOOGLE_CAL_ID
+            }/events`
+          });
+        })
+        .then(
+          response => {
+            // Google cal sorts events by edit time, sorting here by event time
+            let events = response.result.items.sort((a, b) => {
+              return (
+                moment(a.start.dateTime).valueOf() -
+                moment(b.start.dateTime).valueOf()
+              );
+            });
+            that.setState(
+              {
+                events
+              },
+              () => {
+                console.log(that.state.events);
+              }
+            );
+          },
+          function(reason) {
+            console.log(reason);
+          }
+        );
+    }
+    window.gapi.load('client', start);
+  }
+
+  render() {
+    const { events } = this.state;
+    console.log(events[0] ? moment(events[0].start.dateTime).valueOf() : '');
+    return (
+      <div>
+        <p>Countdown</p>
+        <p>{events[0] ? events[0].summary : ''}</p>
+        <p>{events[0] ? events[0].start.dateTime : ''}</p>
+        {events[0]
+          ? this.counter(moment(events[0].start.dateTime).valueOf())
+          : ''}
+      </div>
+    );
+  }
+}
+
+export default Countdown;
