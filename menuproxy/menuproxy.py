f645a6c33c1a356bd1dc0c547f030d301a390558
Kasper Nurminen <kasper@taiste.fi>
2019-02-24 13:36:54 +0200

    Modified flask server

--- menuproxy/menuproxy.py
+++ menuproxy/menuproxy.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from datetime import datetime
 import requests
 
@@ -7,6 +7,8 @@ app = Flask(__name__)
 
 @app.route('/restaurants/')
 def restaurants():
-    timestamp = datetime.now().strftime("%Y/%m/%d");
-    sodexo_url = f'https://kitchen.kanttiinit.fi/restaurants/2/menu?day={timestamp}/';
-    return jsonify({'sodexo': requests.get(sodexo_url).json()})
+    timestamp = datetime.now().strftime("%Y/%m/%d")
+    sodexo_url = f'https://kitchen.kanttiinit.fi/restaurants/2/menu?day={timestamp}/'
+    resp = make_response(jsonify({'sodexo': requests.get(sodexo_url).json()}))
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 import '../App.css';
+import moment from 'moment';
+import axios from 'axios'
+
+
+
 
 class MenuComponent extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            sodexoData: null
+        }
+
+    }
+    componentDidMount() {
+        const apiURL = `http://localhost:3001/restaurants/`;
+        console.log(apiURL)
+        let self = this
+        axios.get(apiURL)
+            .then(function (response) {
+                self.setState({ sodexoData: response.data })
+            })
+    }
     render() {
+        const { sodexoData } = this.state
+        if (!sodexoData) { return null }
+        console.log(sodexoData)
         return (
-            <div class="food-menu">
-                <p> Täält saa ruokaa</p>
+            <div className="food-menu">
+
+
             </div>
         );
     }
