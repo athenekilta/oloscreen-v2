93bfa0984a9195e885508b3655a71eaf1dae3afa
Ilari Kuoppala <ilari@kuoppa.la>
2019-02-24 15:54:41 +0200

    Handle Subway weekends correctly

--- menuproxy/menuproxy.py
+++ menuproxy/menuproxy.py
@@ -15,6 +15,7 @@ def get_json(url):
 @app.route('/restaurants/')
 def restaurants():
     timestamp = datetime.now().strftime("%Y/%m/%d")
+    weekday = datetime.now().weekday()
     amica_timestamp = "&date="
     # Use monday 25.2.19 for testing purposes
     if timestamp == '2019/02/24':
@@ -24,9 +25,16 @@ def restaurants():
     data = {}
     data['sodexo'] = get_json(sodexo_url.format(timestamp))
     data['amica'] = get_json(amica_url + amica_timestamp)
-    data['subway'] = data['sodexo']['menus'][0]['courses'].pop(-1)
+    if len(data['sodexo']['menus']):  # If there are menus on sodexo
+        data['subway'] = data['sodexo']['menus'][0]['courses'].pop(-1)
+    else:
+        if weekday == 5:  # Hardcoded daily sub for Saturdays
+            title = "Kinkku"
+        else:
+            title = ""  # Return empty title when closed
+        data['subway'] = {'title': title, "properties": []}
     data['subway']['title'] = data['subway']['title'].replace("Subway: ","")
-    data['subway']['openingHours'] = subway.get_opening_hours()[datetime.now().weekday()]
+    data['subway']['openingHours'] = subway.get_opening_hours()[weekday]
 
     resp = make_response(jsonify(data))
     resp.headers['Access-Control-Allow-Origin'] = '*'
