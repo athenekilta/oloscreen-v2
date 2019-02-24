578c274b890a1aa3120f89a2bbca45f648854379
Kasper Nurminen <kasper@taiste.fi>
2019-02-24 12:23:23 +0200

    Added MenuComponent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import HeaderComponent from './components/Header.js'
+import MenuComponent from './components/MenuComponent.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <HeaderComponent />
+        <MenuComponent />
       </div>
     );
   }
