da9241419f317b33d408c5b86ec016b7fe77f510
Sampo Rapeli <sampo@rape.li>
2019-02-24 13:37:10 +0200

    Added header files

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,30 @@
-import React, { Component } from 'react';
-import '../App.css';
+import React, { Component } from "react";
+import moment from "moment";
+import "moment/locale/fi";
+import "../App.css";
+import "./header.css";
+
+moment.locale("fi");
 
 class HeaderComponent extends Component {
-    render() {
-        return (
-            <div >
-                <header className="App-header">
-                    <p> Header on tässä </p>
-                </header>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    moment.locale("fi");
+  }
+  render() {
+    console.log(moment.locale("fi"));
+    return (
+      <div>
+        <header className="App-header">
+          <p id="time-date"> {moment().format("LL")} </p>
+          <p id="time-clock"> {moment().format("LTS")} </p>
+          <div id="logo-carousel">
+            <p>Logot</p>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default HeaderComponent;
