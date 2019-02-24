ba6ce3dba6685a398f15fedeffc19ab4fb63a46c
Kasper Nurminen <kasper@taiste.fi>
2019-02-24 12:11:55 +0200

    Added header component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import HeaderComponent from './components/Header.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>
-            Oloscreen v2
-          </h1>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <HeaderComponent />
       </div>
     );
   }
