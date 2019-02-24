37d794e5158e2a24cd348e51cba77ab4f2574511
Ilari Kuoppala <ilari@kuoppa.la>
2019-02-24 13:41:15 +0200

    Improved python proxy startup script

--- menuproxy/start.sh
+++ menuproxy/start.sh
@@ -1,2 +1,8 @@
-source venv/bin/activate
-FLASK_APP=menuproxy.py python3 -m flask run --port 3001
+if [ -d "venv" ]; then
+    source venv/bin/activate
+fi
+if command -v python3 &>/dev/null; then
+    FLASK_APP=menuproxy.py python3 -m flask run --port 3001
+else
+    FLASK_APP=menuproxy.py python -m flask run --port 3001
+fi
