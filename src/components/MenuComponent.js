2c2d2cc8d5de5deec26c8c387e53dc20de0a247a
Kasper Nurminen <kasper@taiste.fi>
2019-02-24 12:34:33 +0200

    Added menucomponent

---
+++ src/components/MenuComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class MenuComponent extends Component {
+    render() {
+        return (
+            <div class="food-menu">
+                <p> Täält saa ruokaa</p>
+            </div>
+        );
+    }
+}
+
+export default MenuComponent;
