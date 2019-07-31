bdab0bba224735c71eac20e40ac93e677c876ce2
Kasper Nurminen <kasper@taiste.fi>
2019-07-31 20:30:08 +0300

    actually Added Shoutboxcomponent

---
+++ src/components/ShoutboxComponent.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class SponsorComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            shouts: []
+        };
+    }
+    componentDidMount() {
+        this.updateShouts()
+        setInterval(this.updateShouts, 5000);
+    }
+    updateShouts = () => {
+        const apiURL = `http://localhost:3001/shoutbox/`;
+        let self = this;
+        axios.get(apiURL).then(function (response) {
+            self.setState({
+                shouts: response.data
+            });
+        });
+    };
+    render() {
+        const { shouts } = this.state
+
+        return (
+            <div id="shouts">
+                <h1>HUUTOLOOTA</h1>
+                <h4> TG: @OloscreenBot</h4>
+                {shouts.map(shout =>
+                    <div>{shout}</div>)}
+
+            </div>
+        );
+    }
+}
+
+export default SponsorComponent;
