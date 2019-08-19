d465b2d6607198adbd3bcea6cb4cc012ad7325b9
Katri Saarinen <katri.saarinen@kaikuhealth.com>
2019-08-19 22:28:31 +0300

    Added dietary codes to menus

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
-import "../App.css";
-import moment from "moment";
-import "moment/locale/fi";
-import axios from "axios";
+import React, { Component } from 'react';
+import '../App.css';
+import moment from 'moment';
+import 'moment/locale/fi';
+import axios from 'axios';
 
-moment.locale("fi");
+moment.locale('fi');
 class MenuComponent extends Component {
   constructor(props) {
     super(props);
@@ -16,66 +16,76 @@ class MenuComponent extends Component {
     const apiURL = `http://localhost:3001/restaurants/`;
     console.log(apiURL);
     let self = this;
-    axios.get(apiURL).then(function (response) {
+    axios.get(apiURL).then(function(response) {
       self.setState({ restaurantData: response.data });
     });
   }
   renderSodexoMenu(restaurantData) {
     const openingHours =
-      restaurantData.openingHours[moment().weekday() - 2] || "Suljettu tänään";
+      restaurantData.openingHours[moment().weekday() - 2] || 'Suljettu tänään';
     const courses = restaurantData.menus[0].courses.map(course => {
-      return <p key={course.title}>{course.title}</p>;
+      return (
+        <p key={course.title}>
+          {course.title}{' '}
+          <span className='foodCodes'>{`(${course.properties})`}</span>
+        </p>
+      );
     });
 
     return (
       <div>
-        <div style={{ display: "flex" }}>
-          <div className="restaurant-title">
-            <h2 className="restaurant"> T-talo</h2>
+        <div style={{ display: 'flex' }}>
+          <div className='restaurant-title'>
+            <h2 className='restaurant'> T-talo</h2>
             <i>{openingHours}</i>
           </div>
 
-          <div className="courses"> {courses}</div>
+          <div className='courses'> {courses}</div>
         </div>
       </div>
     );
   }
+
   renderSubwayMenu(restaurantData) {
     const openingHours = restaurantData.openingHours;
     return (
       <div>
-        <div style={{ display: "flex" }}>
-          <div className="restaurant-title">
-            <h2 className="restaurant"> Subway</h2>
+        <div style={{ display: 'flex' }}>
+          <div className='restaurant-title'>
+            <h2 className='restaurant'> Subway</h2>
             <i>{openingHours}</i>
           </div>
-          <div className="courses" id="subway-courses">
-            {restaurantData.title}{" "}
+          <div className='courses' id='subway-courses'>
+            {restaurantData.title}{' '}
           </div>
         </div>
       </div>
     );
   }
+
   formatCourseName(name) {
-    return name.split(" (")[0];
+    return name.split('(');
   }
+
   renderAmicaMenu(restaurantData) {
-    console.log(restaurantData);
     if (restaurantData.MenusForDays.length === 0) {
-      return null
+      return null;
     }
     const openingHours = restaurantData.MenusForDays[0].LunchTime;
     // eslint-disable-next-line
     const courses = restaurantData.MenusForDays[0].SetMenus.map(course => {
       const courseName = course.Name.toLowerCase();
-      if (courseName.includes("lounas")) {
+      if (courseName.includes('lounas')) {
         return (
-          <div key={course.Components.join(", ")}>
+          <div key={course.Components.join(', ')}>
             <p>
               {course.Components.map(component => {
                 return (
                   <span key={component}>
-                    {this.formatCourseName(component)}
+                    {this.formatCourseName(component)[0]}
+                    <span className='foodCodes'>
+                      {'(' + this.formatCourseName(component)[1]}
+                    </span>
                     <br />
                   </span>
                 );
@@ -83,15 +93,18 @@ class MenuComponent extends Component {
             </p>
           </div>
         );
-      } else if (courseName.includes("bufee")) {
+      } else if (courseName.includes('bufee')) {
         return (
-          <div key={course.Components.join(", ")}>
+          <div key={course.Components.join(', ')}>
             <b>Salaatti</b>
-            <p style={{ marginTop: "6px" }}>
+            <p style={{ marginTop: '6px' }}>
               {course.Components.map(component => {
                 return (
                   <span key={component}>
-                    {this.formatCourseName(component)}
+                    {this.formatCourseName(component)[0]}
+                    <span className='foodCodes'>
+                      {'(' + this.formatCourseName(component)[1]}
+                    </span>
                     <br />
                   </span>
                 );
@@ -101,21 +114,22 @@ class MenuComponent extends Component {
         );
       }
     });
-    const openingHoursParsed = openingHours.split(", "); // split by ','
+
+    const openingHoursParsed = openingHours.split(', '); // split by ','
     return (
       <div>
-        <div style={{ display: "flex" }}>
-          <div className="restaurant-title">
-            <h2 className="restaurant"> TUAS</h2>
+        <div style={{ display: 'flex' }}>
+          <div className='restaurant-title'>
+            <h2 className='restaurant'> TUAS</h2>
             <i>
               <p style={{ margin: 0 }}>{openingHoursParsed[0]}</p>
               <p style={{ marginTop: 0 }}>
-                {openingHoursParsed[1].replace("a la carte", "à la carte")}
+                {openingHoursParsed[1].replace('a la carte', 'à la carte')}
               </p>
             </i>
           </div>
 
-          <div className="courses"> {courses}</div>
+          <div className='courses'> {courses}</div>
         </div>
       </div>
     );
@@ -127,8 +141,8 @@ class MenuComponent extends Component {
     }
     console.log(restaurantData);
     return (
-      <div className="food-menu">
-        <h1 id="menu-title">NÄLKÄ?</h1>
+      <div className='food-menu'>
+        <h1 id='menu-title'>NÄLKÄ?</h1>
         {this.renderSodexoMenu(restaurantData.sodexo)}
         {this.renderAmicaMenu(restaurantData.amica)}
         {this.renderSubwayMenu(restaurantData.subway)}
