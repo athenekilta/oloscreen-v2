85a64918d07e25c6463ecc1829b925b72a32f69d
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2021-02-13 16:08:31 +0200

    Fix #39 and halve logo changing time

--- src/components/SponsorComponent.js
+++ src/components/SponsorComponent.js
@@ -22,15 +22,22 @@ class SponsorComponent extends Component {
       currentLogo: sponsorLogos[0]
     });
 
-    setInterval(this.updateSponsor, 20000);
+    setInterval(this.updateSponsor, 10000);
   }
   updateSponsor = () => {
-    this.setState({
-      currentLogo: this.state.sponsorLogos[
-        (this.state.sponsorLogos.indexOf(this.state.currentLogo) + 1) %
-        this.state.sponsorLogos.length
-      ]
-    });
+    const logoElement = document.getElementById("sponsor-logos");
+    logoElement.classList.add("animate-fade-out");
+    setTimeout(() => {
+      this.setState({
+        currentLogo: this.state.sponsorLogos[
+          (this.state.sponsorLogos.indexOf(this.state.currentLogo) + 1) %
+          this.state.sponsorLogos.length
+        ]
+      });
+      logoElement.classList.remove("animate-fade-out");
+      logoElement.classList.add("animate-fade-in");
+      setTimeout(() => logoElement.classList.remove("animate-fade-in"), 1000);
+    }, 1000);
   };
   render() {
     return (
