27b82567c1a5de065078e2f0861cab954e71fc02
Simo Naatula <git@naatu.la>
2023-06-28 12:00:40 +0300

    Specify html parser

--- backend/get_logos.py
+++ backend/get_logos.py
@@ -3,7 +3,7 @@ import requests
 
 def get_all_logo_links():
     req = requests.get('https://athene.fi', timeout=10)
-    soup = bs(req.text)
+    soup = bs(req.text, 'html.parser')
     sponsor_items = soup.find_all('li', {'class': 'sponsors-item'})
 
     # Parse list of images into a list of img src links
